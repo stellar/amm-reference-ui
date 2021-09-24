c6b177cdc9b9154ef485d1bda044ddff020b33eb
Piyal Basu <pbasu235@gmail.com>
2021-09-24 11:51:10 -0400

    adding mocked known stellar.expert endpoints & payloads using msw (#3)
    * adding mocked known stellar.expert endpoints & payloads using msw
    
    * update stellar.expert AMM url name

---
+++ src/constants/apiUrls.ts
@@ -0,0 +1,1 @@
+export const STELLAR_EXPERT_AMM_URL = "http://amm.stellar.expert";
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
-import * as serviceWorker from "./serviceWorker";
-
+import { worker } from "./mocks/api";
 // Import global CSS from Stellar Design System
 import "@stellar/design-system/build/styles.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+worker.start();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
