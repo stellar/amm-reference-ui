2968009b1e4fd67bdf9bd18bc5549bb4fdb80827
Iveta <quietbits@gmail.com>
2021-10-01 10:50:38 -0400

    Card component

--- src/pages/PoolsOverview.tsx
+++ src/pages/PoolsOverview.tsx
@@ -1,5 +1,19 @@
 import { Layout } from "@stellar/design-system";
+import { Card } from "components/Card";
 
 export const PoolsOverview = () => (
-  <Layout.Inset>Pools overview page</Layout.Inset>
+  <Layout.Inset>
+    Pools overview page
+    <div
+      style={{
+        marginTop: 20,
+        display: "flex",
+        gap: "1rem",
+      }}
+    >
+      <Card>Card content</Card>
+      <Card noPadding>No padding</Card>
+      <Card noShadow>No shadow</Card>
+    </div>
+  </Layout.Inset>
 );
