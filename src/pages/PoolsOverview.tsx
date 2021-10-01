a7b99ee02befcdb950eac9bfad8016bf45960c5c
Iveta <quietbits@gmail.com>
2021-10-01 09:28:08 -0400

    Avatar component

--- src/pages/PoolsOverview.tsx
+++ src/pages/PoolsOverview.tsx
@@ -1,5 +1,50 @@
 import { Layout } from "@stellar/design-system";
+import { Avatar } from "components/Avatar";
 
 export const PoolsOverview = () => (
-  <Layout.Inset>Pools overview page</Layout.Inset>
+  <Layout.Inset>
+    <div>Pools overview page</div>
+
+    <div style={{ marginTop: 20 }}>
+      <Avatar
+        source={[
+          {
+            iconUrl: "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=013",
+            altText: "ETH",
+          },
+        ]}
+      />
+    </div>
+
+    <div style={{ marginTop: 20 }}>
+      <Avatar
+        source={[
+          {
+            iconUrl:
+              "https://cryptologos.cc/logos/usd-coin-usdc-logo.svg?v=013",
+            altText: "USDC",
+            isFullSizeImage: true,
+          },
+        ]}
+      />
+    </div>
+
+    <div style={{ marginTop: 20 }}>
+      <Avatar
+        size="5rem"
+        source={[
+          {
+            iconUrl:
+              "https://cryptologos.cc/logos/usd-coin-usdc-logo.svg?v=013",
+            altText: "USDC",
+            isFullSizeImage: true,
+          },
+          {
+            iconUrl: "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=013",
+            altText: "ETH",
+          },
+        ]}
+      />
+    </div>
+  </Layout.Inset>
 );
