d0d498568d8cebb4b374552970be60c3c3685fab
Iveta <quietbits@gmail.com>
2021-10-04 15:24:09 -0400

    Remove example

--- src/pages/PoolsOverview.tsx
+++ src/pages/PoolsOverview.tsx
@@ -1,42 +1,5 @@
 import { Layout } from "@stellar/design-system";
-import { Breadcrumbs } from "components/Breadcrumbs";
 
 export const PoolsOverview = () => (
-  <Layout.Inset>
-    Pools overview page
-    {/* ./overview/top-liquidity-positions */}
-    <div style={{ marginTop: 20 }}>
-      <Breadcrumbs
-        links={[
-          {
-            link: "./overview",
-            text: "Overview",
-          },
-          {
-            link: "./overview/top-liquidity-positions",
-            text: "Top Liquidity Positions",
-          },
-        ]}
-      />
-    </div>
-    {/* ./overview/pools/USDC-ETH */}
-    <div style={{ marginTop: 20 }}>
-      <Breadcrumbs
-        links={[
-          {
-            link: "./overview",
-            text: "Overview",
-          },
-          {
-            link: "./overview/pools",
-            text: "Pools",
-          },
-          {
-            link: "./overview/pools/USDC-ETH",
-            text: "USDC / ETH",
-          },
-        ]}
-      />
-    </div>
-  </Layout.Inset>
+  <Layout.Inset>Pools overview page</Layout.Inset>
 );
