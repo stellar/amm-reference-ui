f35c31d026ea79b665c9e49055e78963627b4d23
Iveta <quietbits@gmail.com>
2021-10-04 15:18:16 -0400

    Remove example + styled-components

--- src/pages/PoolsOverview.tsx
+++ src/pages/PoolsOverview.tsx
@@ -1,13 +1,5 @@
-import { Layout, Button } from "@stellar/design-system";
-import { Tooltip } from "components/Tooltip";
+import { Layout } from "@stellar/design-system";
 
 export const PoolsOverview = () => (
-  <Layout.Inset>
-    Pools overview page
-    <div style={{ marginTop: 20 }}>
-      <Tooltip position={Tooltip.position.RIGHT} content="Tooltip content">
-        <Button>Show tooltip</Button>
-      </Tooltip>
-    </div>
-  </Layout.Inset>
+  <Layout.Inset>Pools overview page</Layout.Inset>
 );
