bf24cb6045e6688413bbb8dd19a69dbc3c63963e
Iveta <quietbits@users.noreply.github.com>
2021-10-04 15:21:58 -0400

    Avatar component (#6)
    * Avatar component
    
    * Remove broken config link
    
    * Remove example

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,35 @@
+import "./styles.scss";
+
+interface AvatarSource {
+  iconUrl: string;
+  altText: string;
+  backgroundColor?: string;
+  isFullSizeImage?: boolean;
+}
+
+interface AvatarProps {
+  source: AvatarSource[];
+  size?: string;
+  borderColor?: string;
+}
+
+export const Avatar = ({ source, size, borderColor }: AvatarProps) => (
+  <div className="Avatar">
+    {source.map((item) => {
+      const customStyle = {
+        ...(borderColor ? { "--Avatar-border-color": borderColor } : {}),
+        ...(size ? { "--Avatar-size": size } : {}),
+        ...(item.backgroundColor
+          ? { "--Avatar-background-color": item.backgroundColor }
+          : {}),
+        ...(item.isFullSizeImage ? { "--Avatar-image-size": "100%" } : {}),
+      } as React.CSSProperties;
+
+      return (
+        <div key={item.altText} className="Avatar__item" style={customStyle}>
+          <img alt={item.altText} src={item.iconUrl} />
+        </div>
+      );
+    })}
+  </div>
+);
