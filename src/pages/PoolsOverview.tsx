cad5aebd60c259d855339378a7eef40c49af612d
Iveta <quietbits@gmail.com>
2021-10-04 15:19:54 -0400

    Remove example

--- src/pages/PoolsOverview.tsx
+++ src/pages/PoolsOverview.tsx
@@ -1,13 +1,5 @@
 import { Layout } from "@stellar/design-system";
-import { NetworkIndicator } from "components/NetworkIndicator";
-import { StellarNetwork } from "types/types.d";
 
 export const PoolsOverview = () => (
-  <Layout.Inset>
-    <div>Pools overview page</div>
-
-    <div style={{ marginTop: 20 }}>
-      <NetworkIndicator network={StellarNetwork.testnet} />
-    </div>
-  </Layout.Inset>
+  <Layout.Inset>Pools overview page</Layout.Inset>
 );
