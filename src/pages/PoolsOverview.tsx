659479cd36973e29ad8ec9a59142ca3de74d6ff9
Piyal Basu <pbasu235@gmail.com>
2021-10-11 15:52:52 -0400

    rm demo

--- src/pages/PoolsOverview.tsx
+++ src/pages/PoolsOverview.tsx
@@ -1,16 +1,5 @@
 import { Layout } from "@stellar/design-system";
-import { AssetAvatar } from "components/AssetAvatar";
 
 export const PoolsOverview = () => (
-  <Layout.Inset>
-    Pools overview page
-    <div style={{ marginTop: 20 }}>
-      <AssetAvatar
-        assets={[
-          "USDC:GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN",
-          "native",
-        ]}
-      />
-    </div>
-  </Layout.Inset>
+  <Layout.Inset>Pools overview page</Layout.Inset>
 );
