1d4487298083daeef12002dfc23cb0dfda1fa987
Iveta <quietbits@gmail.com>
2021-10-12 09:11:06 -0400

    Remove example

--- src/pages/PoolsOverview.tsx
+++ src/pages/PoolsOverview.tsx
@@ -1,9 +1,5 @@
 import { Layout } from "@stellar/design-system";
-import { PoolTransactionsList } from "components/PoolTransactionsList";
 
 export const PoolsOverview = () => (
-  <Layout.Inset>
-    Pools overview page
-    <PoolTransactionsList poolId="b26c0d6545349ad7f44ba758b7c705459537201583f2e524635be04aff84bc69" />
-  </Layout.Inset>
+  <Layout.Inset>Pools overview page</Layout.Inset>
 );
