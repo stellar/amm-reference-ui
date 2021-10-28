9707b8a6b65f43b916772ba3812efae1731b58f6
Shannon Romano <shannon@stellar.org>
2021-10-28 10:59:09 -0700

    Updated disclaimer in footer (#18)
    * Updated disclaimer in footer
    
    * Adjust footer note styling
    
    Co-authored-by: Iveta <quietbits@gmail.com>

--- src/components/FooterWithNote/index.tsx
+++ src/components/FooterWithNote/index.tsx
@@ -5,7 +5,10 @@ export const FooterWithNote = () => (
   <div className="FooterWithNote">
     <Layout.Inset>
       <div className="FooterWithNote__note">
-        Past performance is not indicative of future results
+        Cryptocurrency assets are subject to high market risks and volatility.
+        Past performance is not indicative of future results. Investments in
+        blockchain assets may result in loss of part or all of your investment.
+        Please do your own research and use caution.
       </div>
     </Layout.Inset>
     <Layout.Footer
