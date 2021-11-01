d7ed8473f46fef22297d618a23d7fae4127dfc65
Piyal Basu <pbasu235@gmail.com>
2021-11-01 13:53:58 -0400

    rm details mock endpoints and add `pools` mocks

--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -1,3 +1,4 @@
-export const STELLAR_EXPERT_AMM_URL = "http://amm.stellar.expert";
+export const STELLAR_EXPERT_AMM_URL =
+  "http://api.stellar.expert/explorer/testnet";
 export const STELLAR_EXPERT_TX = "https://stellar.expert/explorer/testnet/tx";
 export const TESTNET_NETWORK_URL = "https://horizon-testnet.stellar.org";
