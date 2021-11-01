495dfe274fd5af1abd99d859ff3e16196f32d880
Piyal Basu <pbasu235@gmail.com>
2021-11-01 13:59:19 -0400

    use https for api

--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -1,4 +1,4 @@
 export const STELLAR_EXPERT_AMM_URL =
-  "http://api.stellar.expert/explorer/testnet";
+  "https://api.stellar.expert/explorer/testnet";
 export const STELLAR_EXPERT_TX = "https://stellar.expert/explorer/testnet/tx";
 export const TESTNET_NETWORK_URL = "https://horizon-testnet.stellar.org";
