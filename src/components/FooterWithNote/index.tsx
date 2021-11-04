83b66ed4d986f0128068b59858250f07baf24776
Shannon Romano <shannon@stellar.org>
2021-11-04 10:02:20 -0700

    Updated footer text and font size (#26)
    * Updated footer text and font size
    
    * use rem instead of px
    
    Co-authored-by: Piyal Basu <pbasu235@gmail.com>

--- src/components/FooterWithNote/index.tsx
+++ src/components/FooterWithNote/index.tsx
@@ -5,10 +5,23 @@ export const FooterWithNote = () => (
   <div className="FooterWithNote">
     <Layout.Inset>
       <div className="FooterWithNote__note">
-        Cryptocurrency assets are subject to high market risks and volatility.
-        Past performance is not indicative of future results. Investments in
-        blockchain assets may result in loss of part or all of your investment.
-        Please do your own research and use caution.
+        The content and data collected and presented here are made available
+        solely for general informational purposes. The data includes information
+        provided by third parties and we do not warrant its accuracy,
+        usefulness, or completeness. Any reliance you place on such content is
+        strictly at your own risk. We disclaim all liability and responsibility
+        arising from any use of such material by you or any other user of the
+        Services. Trading digital assets is inherently risky and you should
+        consider all of the risks before trading. The value of crypto assets can
+        be extremely volatile and unpredictable, which can result in significant
+        losses in a short time, including possibly a loss of total value. The
+        price and liquidity of crypto assets has been subject to large
+        fluctuations in the past and may be subject to large fluctuations in the
+        future. Historical data is not necessarily indicative of future
+        performance. This information should not be construed as professional
+        advice of any kind -- including investment, financial, trading, tax, or
+        legal advice. You should obtain advice from a suitably qualified
+        professional.
       </div>
     </Layout.Inset>
     <Layout.Footer
