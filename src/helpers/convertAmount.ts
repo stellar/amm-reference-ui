d03e4d8446b046bbdb1944c27294efc0ff5c2e5a
Piyal Basu <pbasu235@gmail.com>
2021-11-19 16:12:15 -0500

    rename helper

--- src/helpers/convertAmount.ts
+++ src/helpers/convertAmount.ts
@@ -5,5 +5,5 @@ type Amount = string | number;
 export const formatAmount = (amount: Amount) =>
   new BigNumber(amount).toFormat();
 
-export const fromStroopsToNumber = (stroops: Amount) =>
+export const fromStroopsToLumen = (stroops: Amount) =>
   Number(new BigNumber(Number(stroops) / 1e7).toFixed(2));
