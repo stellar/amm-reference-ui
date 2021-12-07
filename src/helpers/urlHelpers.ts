a6e1f8f6dbd62c14885ee6a5a3b2f5ad2be4f42d
Piyal Basu <pbasu235@gmail.com>
2021-12-07 17:22:27 -0500

    add routes enum

--- src/helpers/urlHelpers.ts
+++ src/helpers/urlHelpers.ts
@@ -1,1 +1,3 @@
-export const getPoolDetailsUrl = (id: string) => `pool/${id}`;
+import { ROUTES } from "constants/routes";
+
+export const getPoolDetailsUrl = (id: string) => `${ROUTES.POOL_DETAILS}/${id}`;
